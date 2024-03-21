9b076aacfc272760558d39b840fa2ee4043653a5
Nayef Alzayani <nzayani57@outlook.com>
2024-03-21 15:08:38 +0300

    login fixed

--- recipie/models/User.js
+++ recipie/models/User.js
@@ -9,8 +9,8 @@ const IngredientSchema = new Schema({
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
-  avatar: { type: String, required: true },
+  passwordDigest: { type: String, required: true },
+  avatar: { type: String },
   myRecipes: [{ type: Schema.Types.ObjectId, ref: 'Recipe' }],
   savedRecipes: [{ type: Schema.Types.ObjectId, ref: 'Recipe' }],
   shoppingList: [IngredientSchema]
