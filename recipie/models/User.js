9e252a13af81c262ca628bf5ad692b5b7eafe6b3
zynbahmed <201901241@student.polytechnic.bh>
2024-03-24 21:04:49 +0300

    changed the value of the creator in recipe model

--- recipie/models/User.js
+++ recipie/models/User.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose')
+const { Schema } = require("mongoose")
 
 const IngredientSchema = new Schema({
   name: { type: String },
@@ -10,9 +10,13 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   passwordDigest: { type: String, required: true },
-  avatar: { type: String },
-  myRecipes: [{ type: Schema.Types.ObjectId, ref: 'Recipe' }],
-  savedRecipes: [{ type: Schema.Types.ObjectId, ref: 'Recipe' }],
+  avatar: {
+    type: String,
+    default:
+      "https://tr.rbxcdn.com/38c6edcb50633730ff4cf39ac8859840/420/420/Hat/Png"
+  },
+  myRecipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
+  savedRecipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
   shoppingList: [IngredientSchema]
 })
 
