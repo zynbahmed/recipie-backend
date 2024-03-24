7ad22ebb4be298664ca403acd838cc739bbdbfb5
zynbahmed <201901241@student.polytechnic.bh>
2024-03-24 19:41:10 +0300

    added timestamps for recipes

--- recipie/models/Recipe.js
+++ recipie/models/Recipe.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose')
+const { Schema } = require("mongoose")
 
 const IngredientSchema = new Schema({
   name: { type: String },
@@ -6,15 +6,18 @@ const IngredientSchema = new Schema({
   unit: { type: String }
 })
 
-const recipeSchema = new Schema({
-  title: { type: String },
-  description: { type: String },
-  cookingTime: { type: Number },
-  steps: { type: String },
-  photo: { type: String },
-  ingredient: [IngredientSchema],
-  creator: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }]
-})
+const recipeSchema = new Schema(
+  {
+    title: { type: String },
+    description: { type: String },
+    cookingTime: { type: Number },
+    steps: { type: String },
+    photo: { type: String },
+    ingredient: [IngredientSchema],
+    creator: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }]
+  },
+  { timestamps: true }
+)
 
 module.exports = recipeSchema
