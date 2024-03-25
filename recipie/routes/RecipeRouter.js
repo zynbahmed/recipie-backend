db1d0a372c2bb4074f96571975571e8fb750df6e
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-03-26 01:46:31 +0300

    get recipe by category controller and route functional

--- recipie/routes/RecipeRouter.js
+++ recipie/routes/RecipeRouter.js
@@ -3,23 +3,25 @@ const controller = require('../controllers/RecipeController')
 const middleware = require('../middleware')
 
 router.get('/', controller.GetRecipes)
+router.get('/recipesbycat', controller.GetRecipeByCat)
+
 router.get('/:recipe_id', controller.getRecipesDetails)
 router.post(
   '/',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.CreateRecipe
 )
 router.put(
   '/:recipe_id',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.UpdateRecipe
 )
 router.delete(
   '/:recipe_id',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.DeleteRecipe
 )
 
